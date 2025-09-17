0db7ad137bd955fc300e5ba8c3fd5c0cecf84e3f
Gustavo Costa <gustavocosta2407@gmail.com>
2025-09-17 12:33:12 -0300

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MusicList from "./components/MusicList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🎵 Music Streaming App</h1>
+      <MusicList />
+    </div>
+  );
+}
+
+export default App;
